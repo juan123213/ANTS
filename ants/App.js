2ed91d5bb49a85d343bf6f0e895a85ea42dc1f41
scardona55 <sergio.1701910394@ucaldas.edu.co>
2023-10-10 21:19:58 -0500

    Creacion del incio de sesion y inicio del menu

--- ants/App.js
+++ ants/App.js
@@ -1,12 +1,53 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MenuScreen from './menu/menu'; // Importa la pantalla del menú
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Iniciar Sesión' }}
+        />
+        <Stack.Screen
+          name="Menu"
+          component={MenuScreen}
+          options={{ title: 'Menú' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function LoginScreen({ navigation }) {
+  const handleLogin = () => {
+    // Implementa la lógica de inicio de sesión aquí
+    console.log('Usuario:'); // Agrega la lógica que necesites
+
+    // Redirige a la pantalla del menú después del inicio de sesión
+    navigation.navigate('Menu');
+  };
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello world, my name is Sergio </Text>
-      <Text>Este archivo sera para la creacion de nuestra aplicacion movil ANTS</Text>
+      <Text style={styles.projectName}>ANTS</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Usuario"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+      />
+      <Button title="Iniciar Sesión" onPress={handleLogin} style={styles.button} />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +56,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFA500',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  projectName: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#fff',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+  },
+  button: {
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
---
+++ ants/menu/menu.js
@@ -0,0 +1,13 @@
+// menu.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function MenuScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+}
+
+export default MenuScreen;
